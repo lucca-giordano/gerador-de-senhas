f7425c6878ca7fbd954d40672df3e684d74352f0
Lucca Giordano <giordanolucca871@gmail.com>
2023-03-08 18:56:00 -0300

    restarted everything

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,3 @@ function generateRandomPassword(length) {
 }
 
 console.log(generateRandomPassword(12));
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
-
-while(true){
-    console.log(slider.value);
-}

e2702a4b3ef1b17d5c63a7a0359d7f65547e55c7
Lucca Giordano <giordanolucca871@gmail.com>
2023-03-08 18:44:17 -0300

    Range slider

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function generateRandomPassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    let values = new Uint32Array(length);
+    window.crypto.getRandomValues(values);
+    for (let i = 0; i < length; i++) {
+        password += charset[values[i] % charset.length];
+    }
+    return password;
+}
+
+console.log(generateRandomPassword(12));
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+while(true){
+    console.log(slider.value);
+}
